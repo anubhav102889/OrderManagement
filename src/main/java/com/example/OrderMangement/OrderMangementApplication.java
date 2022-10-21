e47a09d77d9b47bc80a9654fc0b8a9af1c77c9b7
anubhav nigam <anubhav.nigam1028@gmail.com>
2022-10-21 11:01:12 +0530

    feature/example 1st commit

--- src/main/java/com/example/OrderMangement/OrderMangementApplication.java
+++ src/main/java/com/example/OrderMangement/OrderMangementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderMangementApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Feature example");
 		SpringApplication.run(OrderMangementApplication.class, args);
 	}
 
